766796fde0391ac7b4f9cd9efdc075f1ac9f6dbc
khairahmard <khairunnisamardiyani@gmail.com>
2017-10-25 02:39:08 +0700

    soal

--- soal5.c
+++ soal5.c
@@ -1,1 +1,16 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h> //library thread
 
+void *print_message_function( void *ptr );
+{
+}
+
+int main()
+{
+     pthread_t thread[50];//inisialisasi awal
+
+
+  exit(EXIT_SUCCESS);
+}
