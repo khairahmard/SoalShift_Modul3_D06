d533e743309dd449f857e343bb5f30797dfce039
khairahmard <khairunnisamardiyani@gmail.com>
2017-10-25 05:20:53 +0700

    soal4

--- soal4.c
+++ soal4.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <pthread.h> //library thread
 
-void *print_message_function( void *ptr );
+void *print_message_function( void *ptr )
 {
      int x=(int)ptr;
      int fac=1;
@@ -13,17 +13,28 @@ void *print_message_function( void *ptr );
 
 int main()
 {
-     
+ 
      pthread_t thread[50];//inisialisasi awal
      int iret;
-
+     int index[50];
+     int jmlindex=0;
      while(1){
-        iret = pthread_create( &thread, NULL, print_message_function, (void*) message);//membuat thread
+        char x;
+        scanf("%d%c",&index[jmlindex],&x);
+        jmlindex++;
+        if(x=='\n') break;
+     }
+
+     for(int i=0;i<jmlindex;i++){
+        iret = pthread_create( &thread[i], NULL, print_message_function, (void*) index[i]);//membuat thread
         if(iret)//jika eror
         {
             fprintf(stderr,"Error - pthread_create() return code: %d\n",iret);
             exit(EXIT_FAILURE);
         }
+     }
+     for(int i=0;i<jmlindex;i++){
+        pthread_join(thread[i], NULL);
      }
 
   exit(EXIT_SUCCESS);
