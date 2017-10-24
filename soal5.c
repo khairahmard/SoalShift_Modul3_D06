596b6868a82c68b8831e4e7da2ceea92c6f70b9d
khairahmard <khairunnisamardiyani@gmail.com>
2017-10-25 03:00:31 +0700

    soal5

--- soal5.c
+++ soal5.c
@@ -10,7 +10,16 @@ void *print_message_function( void *ptr );
 int main()
 {
      pthread_t thread[50];//inisialisasi awal
+     const char *message = "thread";
+     int  iret;
 
+     while(1){
+        iret = pthread_create( &thread, NULL, print_message_function, (void*) message);//membuat thread
+        if(iret)//jika eror
+        {
+            fprintf(stderr,"Error - pthread_create() return code: %d\n",iret);
+            exit(EXIT_FAILURE);
+        }
 
   exit(EXIT_SUCCESS);
 }
